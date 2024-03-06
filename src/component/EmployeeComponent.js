8a86bcfc41eca9c93f00a50da14cbb0b9e73b55a
Juan Osorio Alvarez <116673475+JO503688@users.noreply.github.com>
2024-03-06 11:18:54 -0600

    Update EmployeeComponent.js
    update

--- src/component/EmployeeComponent.js
+++ src/component/EmployeeComponent.js
@@ -25,7 +25,22 @@ componentDidMount(){
 }
   render() {
     return (
-      <div>EmployeeComponent</div>
+      <div>
+        <div className='container card offset-md-3'>
+          <h3 className='text-center card-header'> View Employee Details</h3>
+            <div className='card-body'>
+              <div className='row'>
+                  <p><strong>Employee First Name:</strong>{this.state.employee.firstName}</p>
+              </div>
+              <div className='row'>
+                  <p><strong>Employee Last Name:</strong>{this.state.employee.lastName}</p>
+              </div>
+              <div className='row'>
+                  <p><strong>Employee Email:</strong>{this.state.employee.email}</p>
+              </div>
+            </div>
+        </div>
+      </div>
     )
   }
 }
