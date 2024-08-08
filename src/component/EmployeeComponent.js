a9c7238fff2fcc32d2ccdfb600364fcdf5cd24b7
Faby Pichardo <fb.pichardo@gmail.com>
2024-08-07 21:18:03 -0600

    PIRF: Update

--- src/component/EmployeeComponent.js
+++ src/component/EmployeeComponent.js
@@ -5,38 +5,63 @@ export default class EmployeeComponent extends Component {
 constructor(props) {
     super(props);
     this.state ={
-        employee: {},
-        department: {},
-        organization: {}
+        employeeDto: {},
+        departmentDto: {},
+        organizationDto: {}
     } 
 }
 componentDidMount(){
    EmployeeService.getEmployee().then((Response) =>{
 
-    this.setState({employee: Response.data.employee})
-    this.setState({department : Response.data.department})
-    this.setState({organization : Response.data.organization})
+    this.setState({employeeDto: Response.data.employeeDto})
+    this.setState({departmentDto : Response.data.departmentDto})
+    this.setState({organizationDto : Response.data.organizationDto})
 
-    console.log(this.state.employee)
-    console.log(this.state.department)
-    console.log(this.state.organization)
+    console.log(Response.data.employeeDto)
+    console.log(this.state.employeeDto)
+    console.log(this.state.departmentDto)
+    console.log(this.state.organizationDto)
 
    })
 }
   render() {
     return (
-      <div>
-        <div className='container card offset-md-3'>
+      <div> <br></br>
+        <div className='card col-md-6 offset-md-3'>
           <h3 className='text-center card-header'> View Employee Details</h3>
             <div className='card-body'>
               <div className='row'>
-                  <p><strong>Employee First Name:</strong>{this.state.employee.firstName}</p>
+                  <p><strong>Employee First Name:</strong>{this.state.employeeDto.firstName}</p>
               </div>
               <div className='row'>
-                  <p><strong>Employee Last Name:</strong>{this.state.employee.lastName}</p>
+                  <p><strong>Employee Last Name:</strong>{this.state.employeeDto.lastName}</p>
               </div>
               <div className='row'>
-                  <p><strong>Employee Email:</strong>{this.state.employee.email}</p>
+                  <p><strong>Employee Email:</strong>{this.state.employeeDto.email}</p>
+              </div>
+            </div>
+          <h3 className='text-center card-header'> View Department Details</h3>
+            <div className='card-body'>
+              <div className='row'>
+                  <p><strong>Department Name:</strong>{this.state.departmentDto.departmentName}</p>
+              </div>
+              <div className='row'>
+                  <p><strong>Department Description:</strong>{this.state.departmentDto.departmentDescription}</p>
+              </div>
+              <div className='row'>
+                  <p><strong>Department Code:</strong>{this.state.departmentDto.departmentCode}</p>
+              </div>
+            </div>
+          <h3 className='text-center card-header'> View Organization Details</h3>
+            <div className='card-body'>
+              <div className='row'>
+                  <p><strong>Organization Name:</strong>{this.state.organizationDto.organizationName}</p>
+              </div>
+              <div className='row'>
+                  <p><strong>Organization Description:</strong>{this.state.organizationDto.organizationDescription}</p>
+              </div>
+              <div className='row'>
+                  <p><strong>Organization Code:</strong>{this.state.organizationDto.organizationCode}</p>
               </div>
             </div>
         </div>
